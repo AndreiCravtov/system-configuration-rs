6b6e826821ca8e1d4ac2a62e91fb9fea0e6b0601
Andrei Cravtov <the.andrei.cravtov@gmail.com>
2025-09-17 14:42:15 +0100

    create authorization

--- sys-changer-app/src/main.rs
+++ sys-changer-app/src/main.rs
@@ -1,3 +1,18 @@
+use core_foundation::string::CFString;
+#[cfg(target_os = "macos")]
+use security_framework::authorization::Authorization;
+
+#[cfg(target_os = "macos")]
 pub fn main() {
-    println!("wooo")
+    // constants
+    let proc_name = CFString::new("sys-changer-app");
+    let my_networkset_name = CFString::new("sys-changer-app-networkset");
+
+    // grab authorization
+    let authorization = Authorization::default().unwrap();
+}
+
+#[cfg(not(target_os = "macos"))]
+pub fn main() {
+    panic!("Non-macOS systems are not supported");
 }
