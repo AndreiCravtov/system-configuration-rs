d73d307dc478f063ecf0ca6a8d34426880bd813c
Andrei Cravtov <the.andrei.cravtov@gmail.com>
2025-09-17 18:09:31 +0100

    revert to default

--- sys-changer-app/src/main.rs
+++ sys-changer-app/src/main.rs
@@ -1,7 +1,6 @@
 use core_foundation::string::CFString;
 #[cfg(target_os = "macos")]
 use security_framework::authorization::Authorization;
-use security_framework::authorization::{AuthorizationItemSetStorage, Flags};
 
 #[cfg(target_os = "macos")]
 pub fn main() {
@@ -10,12 +9,7 @@ pub fn main() {
     let my_networkset_name = CFString::new("sys-changer-app-networkset");
 
     // grab authorization
-    let authorization = Authorization::new(
-        None,
-        Some(AuthorizationItemSetStorage::default()),
-        Flags::EXTEND_RIGHTS | Flags::INTERACTION_ALLOWED,
-    )
-    .unwrap();
+    let authorization = Authorization::default().unwrap();
 }
 
 #[cfg(not(target_os = "macos"))]
