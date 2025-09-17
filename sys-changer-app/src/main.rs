c88d822fa61387b91be11577e38163dd86893934
Andrei Cravtov <the.andrei.cravtov@gmail.com>
2025-09-17 18:41:55 +0100

    for SOME reason i had to whip out some sys unsafe code for this thing.... WHYYY?????

--- sys-changer-app/src/main.rs
+++ sys-changer-app/src/main.rs
@@ -1,6 +1,13 @@
+use core_foundation::base::TCFType;
 use core_foundation::string::CFString;
-#[cfg(target_os = "macos")]
-use security_framework::authorization::Authorization;
+use security_framework::base::Error;
+use security_framework_sys::authorization::{
+    errAuthorizationSuccess, kAuthorizationFlagDefaults, AuthorizationCreate, AuthorizationRef,
+};
+use std::mem::MaybeUninit;
+use std::ptr;
+use system_configuration::preferences::SCPreferences;
+use system_configuration_sys::preferences::SCPreferencesCreateWithAuthorization;
 
 #[cfg(target_os = "macos")]
 pub fn main() {
@@ -8,8 +15,28 @@ pub fn main() {
     let proc_name = CFString::new("sys-changer-app");
     let my_networkset_name = CFString::new("sys-changer-app-networkset");
 
-    // grab authorization
-    let authorization = Authorization::default().unwrap();
+    // grab authorization & create preference set with it
+    let mut handle = MaybeUninit::<AuthorizationRef>::uninit();
+    let status = unsafe {
+        AuthorizationCreate(
+            ptr::null(),
+            ptr::null(),
+            kAuthorizationFlagDefaults,
+            handle.as_mut_ptr(),
+        )
+    };
+    if status != errAuthorizationSuccess {
+        Err(Error::from_code(status)).unwrap();
+    }
+    let authorization = unsafe { handle.assume_init() };
+    let preferences = unsafe {
+        SCPreferences::wrap_under_create_rule(SCPreferencesCreateWithAuthorization(
+            ptr::null(),
+            proc_name.as_concrete_TypeRef(),
+            ptr::null(),
+            authorization,
+        ))
+    };
 }
 
 #[cfg(not(target_os = "macos"))]
