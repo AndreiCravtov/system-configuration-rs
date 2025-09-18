ea9b9b7c3be04fb0e7adac27ac84c0ec81d57904
Andrei Cravtov <the.andrei.cravtov@gmail.com>
2025-09-18 18:29:19 +0100

    cut down

--- sys-changer-app/src/tweaking_config.rs
+++ sys-changer-app/src/tweaking_config.rs
@@ -1,16 +1,9 @@
+use crate::ext::CFArrayExt;
 use system_configuration::network_configuration::{SCNetworkService, SCNetworkSet};
 use system_configuration::preferences::SCPreferences;
 
 pub fn remove_bridge_services(prefs: &SCPreferences, set: &SCNetworkSet) {
-    let ifaces = set
-        .services()
-        .into_iter()
-        .map(|i| i.clone())
-        .collect::<Vec<_>>();
-    let iface_priorities = set
-        .service_order()
-        .into_iter()
-        .map(|i| i.clone())
-        .collect::<Vec<_>>();
+    let ifaces = set.services().into_collect::<Vec<_>>();
+    let iface_priorities = set.service_order().into_collect::<Vec<_>>();
     let mut new_ifaces = Vec::<SCNetworkService>::with_capacity(iface_priorities.len());
 }
