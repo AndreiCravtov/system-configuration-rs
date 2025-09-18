63fcb5c986bf609c8d88b4173abc8ee4e5489b3c
Andrei Cravtov <the.andrei.cravtov@gmail.com>
2025-09-18 11:12:35 +0100

    test error handling

--- sys-changer-app/src/main.rs
+++ sys-changer-app/src/main.rs
@@ -1,4 +1,5 @@
 use core_foundation::base::TCFType;
+use core_foundation::error::CFError;
 use core_foundation::string::CFString;
 use security_framework::base::Error;
 use security_framework_sys::authorization::{
@@ -7,7 +8,10 @@ use security_framework_sys::authorization::{
 use std::mem::MaybeUninit;
 use std::ptr;
 use system_configuration::preferences::SCPreferences;
-use system_configuration_sys::preferences::SCPreferencesCreateWithAuthorization;
+use system_configuration_sys::preferences::{
+    SCPreferencesCreateWithAuthorization, SCPreferencesGetValue,
+};
+use system_configuration_sys::system_configuration::SCCopyLastError;
 
 #[cfg(target_os = "macos")]
 pub fn main() {
@@ -37,9 +41,31 @@ pub fn main() {
             authorization,
         ))
     };
+
+    unsafe {
+        let list = SCPreferencesGetValue(
+            preferences.as_concrete_TypeRef(),
+            CFString::new("does_not_exist").as_concrete_TypeRef(),
+        );
+        if list.is_null() {
+            let error = CFError::wrap_under_create_rule(SCCopyLastError());
+            println!("SCPreferencesGetValue returned null w/ error: {:?}", error);
+        }
+    };
 }
 
 #[cfg(not(target_os = "macos"))]
 pub fn main() {
     panic!("Non-macOS systems are not supported");
+}
+
+#[cfg(target_os = "macos")]
+mod helper {
+    use system_configuration::preferences::SCPreferences;
+
+    /// Creates a shallow copy of the provided network set, using the `SCPreferencesPath*` APIs.
+    ///
+    /// The resulting network set will have the exact same __everything__, except for a different
+    /// user-defined name compared to the old network set.
+    fn shallow_clone_network_set(prefs: &SCPreferences) {}
 }
