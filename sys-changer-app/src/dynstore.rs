be258092caddd3f4f02bb964b1e4c183f6147311
Andrei Cravtov <the.andrei.cravtov@gmail.com>
2025-09-19 14:21:10 +0100

    dynstore

--- sys-changer-app/src/dynstore.rs
+++ sys-changer-app/src/dynstore.rs
@@ -1,8 +1,9 @@
+use crate::ext::CFArrayExt;
 use core_foundation::string::CFString;
 use system_configuration::dynamic_store::SCDynamicStoreBuilder;
 
 pub fn dynstore_display(name: &CFString) {
     let dynstore = SCDynamicStoreBuilder::new(name.clone()).build();
-    let keys = dynstore.get_keys(".*").unwrap();
-    println!("keys: {:?}", keys);
+    let keys = dynstore.get_keys(".*").unwrap().into_collect::<Vec<_>>();
+    println!("keys: {:#?}", keys);
 }
