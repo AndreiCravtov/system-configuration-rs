9ae627656c47bde45f15bfa2bedad470ff60e1b9
Andrei Cravtov <the.andrei.cravtov@gmail.com>
2025-09-22 16:46:31 +0100

    generate

--- generate_private_bindings.sh
+++ generate_private_bindings.sh
@@ -44,6 +44,7 @@ SCHEMA_DEFINITIONS_PRIVATE_HEADER_PATH="${SC_HEADER_PATH}/SCSchemaDefinitionsPri
 # ---------------- SystemConfiguration framework bindings ----------------
 SC_BINDING_PATH="./system-configuration-sys/src/private/"
 
+
 DYNAMIC_STORE_PRIVATE_BINDING_PATH="${SC_BINDING_PATH}/dynamic_store_private.rs"
 DYNAMIC_STORE_COPY_SPECIFIC_PRIVATE_BINDING_PATH="${SC_BINDING_PATH}/dynamic_store_copy_specific_private.rs"
 DYNAMIC_STORE_SET_SPECIFIC_PRIVATE_BINDING_PATH="${SC_BINDING_PATH}/dynamic_store_set_specific_private.rs"
